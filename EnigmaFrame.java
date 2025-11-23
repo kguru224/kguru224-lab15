e36cbfc6efceef7bc1f460dc73120473ea5178b7
Kiran Guru <k.guru@gwmail.gwu.edu>
2025-11-23 17:06:42 -0500

    Added Labels and combo boxes

--- EnigmaFrame.java
+++ EnigmaFrame.java
@@ -18,7 +18,25 @@ public class EnigmaFrame extends JFrame {
 
         setSize(650, 450);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new BorderLayout());
+        setLayout(new BorderLayout()); 
+
+        // Top panel for the rotor selection
+
+        JPanel settingPanel = new JPanel();
+        settingsPanel.setLayout(new GridLayout(3,4,10,5));
+
+        // Labels for the rotors and starting letters
+        JLabel innerLabel = new JLabel("Inner Rotor");
+        JLabel middleLabel = new JLabel("Middle Rotor");
+        JLabel outerLabel = new JLabel("Outer Rotor");
+        JLabel startLabel = new JLabel("Starting Latters");
+
+        // Combo boxes for the rotors selection
+
+        Integer[] rotorChoices = {1,2,3,4,5};
+        innerBox = new JComboBox<>(rotorChoices);
+        middleBox = new JComboBox<>(rotorChoices);
+        outerBox = new JComboBox<>(rotorChoices);
     }
 
 
